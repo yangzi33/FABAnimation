566457cfdf894a3ccc669fc52a2e198ab380b5e0
yangzi33 <ziyue.yang@mail.utoronto.ca>
2020-04-11 03:37:45 -0400

    Create SQLite project

---
+++ DatabasePlayground/app/src/main/java/com/example/databaseplayground/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.databaseplayground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
